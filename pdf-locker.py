3b30abe2dd7b2b0eb0b7a6a2d02217bb5155820a
Jorge CP <64277389+jp2435@users.noreply.github.com>
2021-04-18 12:50:39 +0100

    Create pdf-locker.py

---
+++ pdf-locker.py
@@ -0,0 +1,18 @@
+import PyPDF2 import PdfFileWriter, PdfFileReader
+
+out = PdfFileWriter()
+
+file_path = "sample.pdf"
+file = PdfFileReader(file_path)
+
+num = file.numPages
+for idx in range(num):
+  page = file.getPage(idx)
+  out.addPage(page)
+  
+password = "123456pass"
+
+out.encrypt(password)
+
+with open("encrypted_file.pdf", "wb") as f:
+  out.write(f)
